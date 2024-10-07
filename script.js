b676714bb96ad34c95281f568db68d5cda7561a5
Tobias Vinther <Tobias.Vinther88@gmail.com>
2024-10-07 14:11:15 +0200

    added some responsiveness for small screen-sizes

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
+
+// Variable declarations
 const rightAnswere = Math.round(Math.random() * 100);
-console.log(rightAnswere);
 let guess = document.querySelector('#input');
-let button = document.querySelector('.btn');
 let prompt = document.querySelector('#prompt');
 let lastGuess = document.querySelector('#last-guess');
 let clue = document.querySelector('#clue');
 let totalGuesses = document.querySelector('#total-guesses');
+let total = 0;
+// Variable declarations
 
+// Global functions
 function isBetween0And100(value) {
   const regex = /^(100|[1-9]?[0-9])$/;
   return regex.test(value);
@@ -20,32 +23,33 @@ function promptMessage(color, text){
     prompt.style.opacity = 0;
 }, 5000);
 }
+// Global functions
 
-let total = 0;
-button.addEventListener('mouseup', ()=>{
 
+document.addEventListener('keypress', function (e) {
+  if (e.key === 'Enter') {
   if(isBetween0And100(guess.value)){
     lastGuess.textContent = guess.value;
     if(guess.value == rightAnswere){
-      promptMessage('#67c787','You won! Refresh page to try again');
+      promptMessage('#67c787','You won! Refresh page (F5) to try again');
       clue.textContent = 'Winner';
       clue.style.color = 'var(--clr-success)';
       
       total++;
       totalGuesses.textContent = total;
     }else if( guess.value < rightAnswere){
-      clue.textContent = 'Higher';
+      clue.textContent = 'Low';
       clue.style.color = 'var(--clr-warning)';
       total++;
       totalGuesses.textContent = total;
     }else {
-      prompt.textContent = '';
-      clue.textContent = 'Lower';
+      clue.textContent = 'High';
       clue.style.color = 'var(--clr-info)';
       total++;
       totalGuesses.textContent = total;
     }
   }else{
     promptMessage('#c76767','You must enter a value between 0-100');
+  }
   }
 })
