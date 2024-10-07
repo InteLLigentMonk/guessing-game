6a78cefa8b5052e344c2167e7f01dc894e17b6e7
Tobias Vinther <Tobias.Vinther88@gmail.com>
2024-10-07 14:46:26 +0200

    added coloring for total guesses, 1-5 green, 6-10 orange, 11< red

--- script.js
+++ script.js
@@ -28,28 +28,41 @@ function promptMessage(color, text){
 
 document.addEventListener('keypress', function (e) {
   if (e.key === 'Enter') {
-  if(isBetween0And100(guess.value)){
-    lastGuess.textContent = guess.value;
-    if(guess.value == rightAnswere){
-      promptMessage('#67c787','You won! Refresh page (F5) to try again');
-      clue.textContent = 'Winner';
-      clue.style.color = 'var(--clr-success)';
+    if(isBetween0And100(guess.value)){
       
-      total++;
-      totalGuesses.textContent = total;
-    }else if( guess.value < rightAnswere){
-      clue.textContent = 'Low';
-      clue.style.color = 'var(--clr-warning)';
-      total++;
-      totalGuesses.textContent = total;
-    }else {
-      clue.textContent = 'High';
-      clue.style.color = 'var(--clr-info)';
-      total++;
-      totalGuesses.textContent = total;
+      // Change color of Total depending on how many tries.
+      if(total <= 4){
+        totalGuesses.style.color ='var(--clr-success)';
+      } else if(total > 4 && total <=9){
+        totalGuesses.style.color ='var(--clr-warning)';
+      } else{
+        totalGuesses.style.color ='var(--clr-error)';
+      }
+
+      // Update the last guess field to the entered value.
+      lastGuess.textContent = guess.value;
+      
+      
+      if(guess.value == rightAnswere){
+        promptMessage('#67c787','You won! Refresh page (F5) to try again');
+        clue.textContent = 'Winner';
+        clue.style.color = 'var(--clr-success)';
+        total++;
+        totalGuesses.textContent = total;
+      }else if( guess.value < rightAnswere){
+        clue.textContent = 'Low';
+        clue.style.color = 'var(--clr-warning)';
+        total++;
+        totalGuesses.textContent = total;
+      }else {
+        clue.textContent = 'High';
+        clue.style.color = 'var(--clr-info)';
+        total++;
+        totalGuesses.textContent = total;
+      }
+    }else{
+      promptMessage('#c76767','You must enter a value between 0-100');
     }
-  }else{
-    promptMessage('#c76767','You must enter a value between 0-100');
-  }
   }
 })
+
